4181bcc4397279aba72237f9a77addd0702f6fda
shoaib812 <Shoaib@78400>
2022-09-27 15:39:22 +0530

    git commit-m "JavaMatrix/src/Main/MatrixMultiplication3"

---
+++ JavaMatrix/src/MatrixMultiplication3.java
@@ -0,0 +1,9 @@
+public class MatrixMultiplication3
+{
+    public static void main(String[]args)
+    {
+        int mat1[][] = {{1, 3, 4}, {2, 4, 3}, {3, 4, 5}};
+        int mat2[][] = {{1, 3, 4}, {2, 4, 3}, {1, 2, 4}};
+        int mat3[][] = new int[3][3];
+    }
+}
