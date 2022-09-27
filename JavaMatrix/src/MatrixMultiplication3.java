a16cc08f28ad98e009566bfabac603b6a835eb46
shoaib812 <Shoaib@78400>
2022-09-27 15:51:21 +0530

    git pull-m "JavaMatrix/src/Main/MatrixMultiplication3"

--- JavaMatrix/src/MatrixMultiplication3.java
+++ JavaMatrix/src/MatrixMultiplication3.java
@@ -2,8 +2,24 @@ public class MatrixMultiplication3
 {
     public static void main(String[]args)
     {
-        int mat1[][] = {{1, 3, 4}, {2, 4, 3}, {3, 4, 5}};
-        int mat2[][] = {{1, 3, 4}, {2, 4, 3}, {1, 2, 4}};
+        int mat1[][] = {{1,1,1},{2,2,2},{3,3,3}};
+        int mat2[][] = {{1,1,1},{2,2,2},{3,3,3}};
         int mat3[][] = new int[3][3];
+
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                mat3[i][j]=0;
+
+                for(int k=0; k<3; k++)
+                {
+                    mat3[i][j] += mat1[i][k] * mat2[k][j];
+                }
+
+                System.out.print(mat3[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
